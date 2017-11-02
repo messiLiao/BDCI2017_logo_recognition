a5fd6e27039ba9892e806c08ac3ff56062992a3a
messiliao <bh_binghu@163.com>
2017-11-02 14:19:40 +0800

    delete no useful file

--- valid.sh
+++ valid.sh
@@ -1,5 +1,14 @@
-./darknet detector valid cfg/bdci2017.data cfg/bdci2017_test.cfg backup/bdci2017_train.backup 
+./darknet detector valid cfg/bdci2017.data cfg/bdci2017_test.cfg backup/bdci2017_train_900.backup 
 echo ""
 echo "----------valid finished------------"
 echo ""
+tar -czvf results.tar.gz ./results/
+echo ""
+echo "----------tar dirctory results finished"
+echo ""
+baidupcs upload -f results.tar.gz results.tar.gz
+echo ""
+echo "-------upload results.tar.gz finised-----"
+echo ""
+
 
