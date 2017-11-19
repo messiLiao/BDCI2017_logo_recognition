28637592b0d00f21669c1d9967cbd5fe0dda4081
messiLiao <krobot.lmx@gmail.com>
2017-11-19 11:42:37 +0800

    use multiprocess to generate images

--- dataset/create_random_images.py
+++ dataset/create_random_images.py
@@ -4,6 +4,7 @@ import os
 import cv2
 import numpy as np
 import random
+import multiprocessing
 
 def show_mask(image_dir):
     image_fn_list = os.listdir(image_dir)
@@ -183,6 +184,129 @@ def get_random_alpha_str(length):
     return ''.join(s)
 
 
+def SynthProcess(image_fn, mask_fn, bg_image_fn, output_dir, class_id):    
+
+    min_side = 30
+    max_side = 100
+    min_kernel_size = 1
+    max_kernel_size = 15
+
+    image_output_name = "synth_images"
+    label_output_name = "synth_labels"
+
+    image_output_dir = os.path.join(output_dir, image_output_name)
+    label_output_dir = os.path.join(output_dir, label_output_name)
+    image = cv2.imread(image_fn)
+    mask = cv2.imread(mask_fn, 0)
+    bg_image = cv2.imread(bg_image_fn)
+
+    if (image is None) or (mask is None) or (bg_image is None):
+        return None
+
+    mask = erode_or_dilate_mask(mask, min_kernel_size, max_kernel_size)
+
+    image, mask = perspective(image, mask, min_side, max_side)
+
+    output_image, bbox = merge_background(image, mask, bg_image)
+
+    _, fn = os.path.split(bg_image_fn)
+    name, ext = os.path.splitext(fn)
+
+    random_alpha_str = get_random_alpha_str(8)
+    output_image_fn = os.path.join(image_output_dir, '%s_%s%s' % (name, random_alpha_str, ext))
+    output_label_fn = os.path.join(label_output_dir, '%s_%s.txt' % (name, random_alpha_str))
+
+    result = "%s %s\n" % (os.path.join(image_output_name, os.path.split(output_image_fn)[1]),
+                                        os.path.join(label_output_name, os.path.split(output_label_fn)[1]))
+
+    cv2.imwrite(output_image_fn, output_image)
+    line = None
+    with open(output_label_fn, 'w') as fd:
+        line = "%s %d " % (os.path.split(output_image_fn)[1], class_id)
+        line += "%d %d %d %d" % bbox
+        fd.write(line)
+    return result
+
+def generate_syntm_images(image_dir, mask_dir, background_dir, output_dir, generate_cnt, debug=False):
+    label_class_id = dict()
+    label_class_id['beiqi'] = 1
+    label_class_id['ford'] = 2
+    label_class_id['skoda'] = 3
+    label_class_id['qichen'] = 4
+    label_class_id['honda'] = 5
+    label_class_id['nissan'] = 6
+    label_class_id['cadlock'] = 7
+    label_class_id['linmu'] = 8
+    label_class_id['geely'] = 9
+    label_class_id['geely1'] = 9
+    label_class_id['posche'] = 10
+    label_class_id['jeep'] = 11
+    label_class_id['jeep1'] = 11
+    label_class_id['baojun'] = 12
+    label_class_id['ronwei'] = 13
+    label_class_id['linken'] = 14
+    label_class_id['toyota'] = 15
+    label_class_id['buik'] = 16
+    label_class_id['qirui'] = 17
+    label_class_id['kia'] = 18
+    label_class_id['haval'] = 19
+    label_class_id['audi'] = 20
+    label_class_id['randrover'] = 21
+    label_class_id['dasauto'] = 22
+    label_class_id['chuanqi'] = 23
+    label_class_id['changan'] = 24
+    label_class_id['mg'] = 25
+    label_class_id['leno'] = 26
+    label_class_id['lexs'] = 27
+    label_class_id['bmw'] = 28
+    label_class_id['mazida'] = 29
+    label_class_id['benz'] = 30
+
+    image_fn_list = os.listdir(image_dir)
+    bg_image_fn_list = os.listdir(background_dir)
+
+    image_cnt = 0
+
+    cpu_count = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=cpu_count)
+    print "cpu_count = %d" % cpu_count
+
+    image_output_name = "synth_images"
+    label_output_name = "synth_labels"
+
+    image_output_dir = os.path.join(output_dir, image_output_name)
+    label_output_dir = os.path.join(output_dir, label_output_name)
+    
+    if not os.path.isdir(image_output_dir):
+        os.mkdir(image_output_dir)
+    if not os.path.isdir(label_output_dir):
+        os.mkdir(label_output_dir)
+
+    list_output_fn = os.path.join(output_dir, "synth_logo_list.txt")
+    list_output_fd = open(list_output_fn, 'w')
+    while image_cnt < generate_cnt:
+        image_cnt += 1
+        image_fn = random.choice(image_fn_list)
+
+        name, ext = os.path.splitext(image_fn)
+        image_fn = os.path.join(image_dir, image_fn)
+        mask_fn = os.path.join(mask_dir, "%s.bmp" % name)
+
+        class_id = label_class_id[name]
+
+
+        bg_image_fn = random.choice(bg_image_fn_list)
+        bg_image_fn = os.path.join(background_dir, bg_image_fn)
+        
+        result = pool.apply_async(SynthProcess, (image_fn, mask_fn, bg_image_fn, output_dir, class_id))
+        line = result.get()
+        list_output_fd.write(line)
+
+    pool.close()
+    pool.join()
+    list_output_fd.close()
+    pass
+
 def main(image_dir, mask_dir, background_dir, output_dir, generate_cnt, debug=False):
     label_class_id = dict()
     label_class_id['beiqi'] = 1
@@ -231,6 +355,8 @@ def main(image_dir, mask_dir, background_dir, output_dir, generate_cnt, debug=Fa
     max_side = 100
     min_kernel_size = 1
     max_kernel_size = 15
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
     if not os.path.isdir(image_output_dir):
         os.mkdir(image_output_dir)
     if not os.path.isdir(label_output_dir):
@@ -285,16 +411,17 @@ def main(image_dir, mask_dir, background_dir, output_dir, generate_cnt, debug=Fa
 
 
 if __name__ == '__main__':
-    image_dir = './logos'
-    mask_dir = './logos_mask'
-    background_dir = './background_images'
-    output_dir = './'
-    generate_cnt = 100
+    image_dir = 'logos'
+    mask_dir = 'logos_mask'
+    background_dir = 'background_images'
+    output_dir = '.'
+    generate_cnt = 10
 
     # image_dir = './logos'
     # mask_dir = './logos_mask'
     # background_dir = './background_images'
     # output_dir = '/home/xbn/work/kongchang/data'
-    # generate_c nt = 10
+    # generate_cnt = 10
 
-    main(image_dir, mask_dir, background_dir, output_dir, generate_cnt, debug=False)
+    # main(image_dir, mask_dir, background_dir, output_dir, generate_cnt, debug=False)
+    generate_syntm_images(image_dir, mask_dir, background_dir, output_dir, generate_cnt, debug=True)
